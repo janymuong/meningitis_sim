e1b00c518d230eee9482c902a1d369b11159a0fc
Jany Muong <me.roumuong@gmail.com>
2024-07-01 10:52:24 +0300

    comment urls - views

--- seirs_sim/urls.py
+++ seirs_sim/urls.py
@@ -4,10 +4,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # normal simulation route/view
     path('normal_simulation/', views.normal_simulation, name='normal_simulation'),
+    # view for model visualizations:
     path('normal_simulation_result/', views.normal_simulation_result, name='normal_simulation_result'),
-    path('vaccine_simulation/', views.vaccine_simulation, name='vaccine_simulation'),  # view for vaccine simulation
-    # view for vaccine simulation result
+    # view for vaccine simulation:
+    path('vaccine_simulation/', views.vaccine_simulation, name='vaccine_simulation'),
+    # view for vaccine simulation visualizations
     path('vaccine_simulation_result/<str:probs>/', views.vaccine_simulation_result, name='vaccine_simulation_result'), 
 ]
 
