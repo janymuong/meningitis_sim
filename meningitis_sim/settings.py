d3ebc3c82cc2029350c705aea34d3cd8d065aeb1
Jany Muong <me.roumuong@gmail.com>
2024-07-31 15:43:38 +0300

    deployment setup

--- meningitis_sim/settings.py
+++ meningitis_sim/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@7pzf&8inzm2ov&a75o)868k0%+kukyxby93))l&638io$k9j-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
